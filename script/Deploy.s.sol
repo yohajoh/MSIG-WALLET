12d00d2c976adca3c944b3ce0b93246cd49c853f
yohajoh <ybelete490@gmail.com>
2026-05-16 14:59:13 -0400

    Remove commented names from owners array in DeployMultiSig contract
    Signed-off-by: yohajoh <ybelete490@gmail.com>

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -11,9 +11,9 @@ contract DeployMultiSig is Script {
         // ================= OWNERS =================
         address[] memory owners = new address[](3);
 
-        owners[0] = 0xaA7827FF19A96231f6582A830f3df8ea8aF4cfB9; // tsion
-        owners[1] = 0x6c856fc3768Bb3715015c949273eA2CE6B1ad4BE; // salah
-        owners[2] = 0x255e59557Ccd5bEFb82e2c5F72E61768713a8a74; // lina
+        owners[0] = 0xaA7827FF19A96231f6582A830f3df8ea8aF4cfB9; 
+        owners[1] = 0x6c856fc3768Bb3715015c949273eA2CE6B1ad4BE; 
+        owners[2] = 0x255e59557Ccd5bEFb82e2c5F72E61768713a8a74; 
 
         // ================= THRESHOLD =================
         uint256 requiredConfirmations = owners.length - 1; 
